bc58d22c223fddfce9324fc02347d3b661ba405b
Michael Pedersen <mp@oxygen.dk>
2020-03-03 09:56:03 +0100

    Step 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
-﻿using GradeBook.Enums;
+﻿using System;
+using System.Linq;
+using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +9,45 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+
+            var orderedStudents = Students.OrderBy(x => x.Grades);
+
+            var numberOfStudents = Students.Count;
+            var betterThanThisStudent = 0;
+            for (int i = 0; i < numberOfStudents; i++)
+            {
+                if (averageGrade < Students[i].AverageGrade)
+                {
+                    continue;
+                }
+                else
+                {
+                    betterThanThisStudent = i + 1;
+                    break;
+                }
+            }
+
+            double percentile = (double)betterThanThisStudent / numberOfStudents;
+            if (percentile <= 0.20)
+                return 'A';
+            if (percentile > 0.20 && percentile <= 0.40)
+                return 'B';
+            if (percentile > 0.40 && percentile <= 0.60)
+                return 'C';
+            if (percentile > 0.60 && percentile <= 0.80)
+                return 'D';
+            if (percentile > 0.80)
+                return 'F';
+
+            return 'F';
         }
     }
 }
